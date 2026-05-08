83d3eb4a426354e928a4deefa2af54673cc1d472
Jaeheon Lee <ymkzz123@gmail.com>
2026-05-08 14:32:10 +0900

    [Passed] 특정 조건 두 정수 비교 (8ms, 0MB)

---
+++ trail0/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    if (A<B) cout << 1;
+    else cout << 0;
+    cout << " ";
+    if (A==B) cout << 1;
+    else cout << 0;
+    
+    return 0;
+}
