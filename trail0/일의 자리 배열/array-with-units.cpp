cac0db55b2a01668dcf6a37edccde9027092c2be
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-06 19:21:13 +0900

    [Passed] 일의 자리 배열 (2ms, 0MB)

--- trail0/일의 자리 배열/array-with-units.cpp
+++ trail0/일의 자리 배열/array-with-units.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    
+
+    int lst[11] = {};
     int a, b;
     cin >> a >> b;
-
-    int lst[10] = {a, b};
-    for (int i = 2; i<10; i++) {
-        lst[i] = (lst[i-1]+lst[i-2])%10;
+    lst[0] = a;
+    lst[1] = b;
+    for (int i = 2; i < 10; i++) {
+        lst[i] = (lst[i-2]+lst[i-1])%10;
     }
 
-    for (int i = 0; i<10; i++) {
+    for (int i = 0; i < 10; i++) {
         cout << lst[i] << " ";
     }
-
 
     return 0;
 }
