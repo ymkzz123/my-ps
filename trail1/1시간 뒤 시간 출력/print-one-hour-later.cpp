62b16cba312ae760d2246066c6d5c22b26685e84
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-07 17:34:53 +0900

    [Passed] 1시간 뒤 시간 출력 (2ms, 0MB)

---
+++ trail1/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int h, m;
+    char c;
+    cin >> h;
+    cin.get();
+    cin >> m;
+    cout << h+1 << ":" << m;
+    
+    return 0;
+}
