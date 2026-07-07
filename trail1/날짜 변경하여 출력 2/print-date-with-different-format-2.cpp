9b9c9327f33bf62c459f22c6618c04841c731cd7
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-07 17:48:24 +0900

    [Passed] 날짜 변경하여 출력 2 (1ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int y, m, d;
+    char fuck;
+    cin >> m >> fuck >> d >> fuck >> y;
+    cout << y << "." << m << "." << d;
+    return 0;
+}
