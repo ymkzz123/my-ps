ce77bfd6b58a83435bdadf4c554adeabae007421
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-07 17:52:07 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    char fuck;
+    cin >> a >> fuck >> b;
+    cout << a << b;
+    return 0;
+}
