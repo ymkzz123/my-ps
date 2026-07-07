c4ec1113e5737ac0df894cad55558b15ad78ebc1
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-07 22:48:19 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int fuck, x, y;
+    
+    cin >> fuck;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    cout << "010-" << y << "-" << x;
+    return 0;
+}
