789a168676925b4c6803d77e4a2651eb106419b4
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-07 16:58:05 +0900

    [Passed] 정수 입력받아 계산 (3ms, 0MB)

---
+++ trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    a *= 2;
+    a += 3;
+    cout << a;
+    return 0;
+}
