f5d32867a14f3689266b5b9503f15e4ce0276965
Jaeheon Lee <ymkzz123@gmail.com>
2026-07-19 23:16:12 +0900

    [Passed] 물의 상태 (2ms, 0MB)

---
+++ trail0/물의 상태/state-of-water.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int temperature;
+    cin >> temperature;
+
+    if (temperature < 0) cout << "ice";
+    else if (temperature > 99) cout << "vapor";
+    else cout << "water";
+    return 0;
+}
